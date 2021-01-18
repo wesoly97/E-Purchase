44f3685941a2507887f97b62e715779389166ec7
wesoly97 <61495739+wesoly97@users.noreply.github.com>
2021-01-18 19:39:50 +0100

    route account

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -5,7 +5,15 @@ const Navbar = () => {
    return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
+           {/*   
+                <div id="register">
                 <Link to="/register" className="brand-logo">E-Purchase</Link>
+                </div>
+                */}
+                <div id="account">
+                <Link to="/account" >Konto</Link>
+                </div>
+               
             </div>
         </nav>
    );
