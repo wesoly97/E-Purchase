2279380a1b78e08ce28a7e50d65a593391f522bd
Kacper Murczkiewicz <murczkiewiczkacper@gmail.com>
2021-01-21 18:48:09 +0100

    Auctions_minor_fix

--- client/src/pages/Auctions.js
+++ client/src/pages/Auctions.js
@@ -3,6 +3,9 @@ import Axios from "axios";
 
 import { useHistory } from 'react-router-dom';
 import Navbar from "../layout/Navbar";
+import "../styles/Auctions.css"
+import $ from "jquery"
+import M from "materialize-css"
 
 export default function Main(){
 
@@ -33,20 +36,27 @@ export default function Main(){
         history.push("/addAuction");
     };
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems);
+    });
+
 
     if(typeof(itemsToShow[0]) !== 'undefined' && itemsToShow[0] != null)
     {
         return (
-            <div>
+            <div className="row">
 
                 <Navbar/>
-                <h1>AUKCJE</h1>
-                <button onClick={addAuction} className="btn waves-effect waves-light" type="submit" name="action">Dodaj
-                    aukcję
-                    <i className="material-icons right">send</i>
-                </button>
-
-                <div>
+                <div className="col s4">
+                    <h1>AUKCJE</h1>
+                    <button onClick={addAuction} className="btn waves-effect waves-light" type="submit" name="action">Dodaj
+                        aukcję
+                        <i className="material-icons right">send</i>
+                    </button>
+                </div>
+                {/* <div className="col s8">
+                    Jakis ladny froncik
                     {itemsToShow.map((item, index) => (
                         <div key={index}>
                             <p>{item.name}</p>
@@ -56,7 +66,34 @@ export default function Main(){
                             <p>============================</p>
                         </div>
                     ))}
+
+                </div>*/}
+
+                <div className="col s8">
+                    <div className="listWrapper">
+                        <div id="opinions" className="right">
+                        <ul className="collection">
+                            {/*listing of items*/}
+                            {itemsToShow.map((item, index) => (
+                                <li className="collection-item ">
+                                    <div key={index}>
+                                        <p><b>Nazwa:</b>    {item.name}</p>
+                                        <p><b>Opis:</b>     {item.description}</p>
+                                        <p><b>Ilosc:</b>    {item.quantity}szt</p>
+                                        <p><b>Cena:</b>     {item.price}zł</p>
+                                        {/*WYSKAKUJECE OKIENKO Z INFO*/}
+                                        <button data-target="modal1" className="btn modal-trigger">Modal</button>
+                                        {/*WYSKAKUJECE OKIENKO Z INFO*/}
+
+                                    </div>
+                                </li>
+
+                            ))}
+                        </ul>
+                    </div>
                 </div>
+
+    </div>
 
 
             </div>
